35216d6888da3735370b35a159210360ceea03d2
jose mejia <jose mejia@AlejandroHP>
2016-04-28 02:03:34 -0400

    Configuraciones Iniciales v1

---
+++ core/src/main/java/org/sw/andromda/lastrest/service/SearchServiceImpl.java
@@ -0,0 +1,43 @@
+// license-header java merge-point
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ * TEMPLATE:    SpringServiceImpl.vsl in andromda-spring cartridge
+ * MODEL CLASS: AndroMDAModel::org.sw.andromda.lastrest::service::SearchService
+ * STEREOTYPE:  Service
+ * STEREOTYPE:  WebService
+ */
+package org.sw.andromda.lastrest.service;
+
+import java.util.Set;
+import org.sw.andromda.lastrest.vo.DocumentVO;
+import org.sw.andromda.lastrest.vo.SearchResultVO;
+
+/**
+ * @see org.sw.andromda.lastrest.service.SearchService
+ */
+public class SearchServiceImpl
+    extends SearchServiceBase
+{
+
+    /**
+     * @see org.sw.andromda.lastrest.service.SearchService#performSearch(String)
+     */
+    protected  SearchResultVO handlePerformSearch(String queryTerm)
+        throws Exception
+    {
+        // TODO implement protected  SearchResultVO handlePerformSearch(String queryTerm)
+        throw new UnsupportedOperationException("org.sw.andromda.lastrest.service.SearchService.handlePerformSearch(String queryTerm) Not implemented!");
+    }
+
+    /**
+     * @see org.sw.andromda.lastrest.service.SearchService#indexDocument(Set<DocumentVO>)
+     */
+    protected  Boolean handleIndexDocument(Set<DocumentVO> documents)
+        throws Exception
+    {
+        // TODO implement protected  Boolean handleIndexDocument(Set<DocumentVO> documents)
+        throw new UnsupportedOperationException("org.sw.andromda.lastrest.service.SearchService.handleIndexDocument(Set<DocumentVO> documents) Not implemented!");
+    }
+
+}
